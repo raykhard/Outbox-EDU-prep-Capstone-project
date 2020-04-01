a609170b5ee771857f2ba98fe09a78d6b4638dd3
raykhard <raykhardtaremwa@gmail.com>
2020-04-01 11:11:48 +0300

    updated-directory

---
+++ UI/script/index.js
@@ -0,0 +1,30 @@
+function Validate()                                    
+{ 
+    var name = document.forms["forms"]["Name"]; 
+    console.log (name.value)
+    var email = document.forms["forms"]["email"]; 
+    var password = document.forms["forms"]["password"];    
+   
+    if (name.value == "")                                  
+    { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+    }  
+       
+    if (email.value == "")                                   
+    { 
+        window.alert("Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+   
+    if (password.value == "")                        
+    { 
+        window.alert("Please enter your password"); 
+        password.focus(); 
+        return false; 
+    } 
+
+    return true; 
+}
