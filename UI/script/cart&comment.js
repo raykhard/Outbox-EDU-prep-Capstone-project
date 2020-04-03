5d440453d49d2c432c21e72cd588b39863f1aa97
raykhard <raykhardtaremwa@gmail.com>
2020-04-03 14:49:42 +0300

    cart

--- UI/script/cart&comment.js
+++ UI/script/cart&comment.js
@@ -1,80 +1,65 @@
-// function passvalues()
-// {
-//     var firstname=document.getElementById("videos").value;
-//     localStorage.setItem("video/mp4value",firstname);
-//     return false;
-// }
-// nav-bar
+
 function toggleNav(){
     document.querySelector(".access-point").classList.toggle("access-point--open");
 }
+//COMMENT
 
+//END OF COMMENT
 let carts = document.querySelectorAll('.add-cart');
 
 let products = [
     {
-        name: 'Corona Fear',
+        name: 'grey tshirt',
         tag: 'greytshirt',
-        price: 15,
+        prices: 15,
         inCart:0
     },
     {
-        name: 'Corona Fear',
+        name: 'grey tshirt',
         tag: 'greytshirt',
-        price: 15,
+        prices: 15,
         inCart:0
     },
     {
-        name: 'Corona Fear',
+        name: 'grey tshirt',
         tag: 'greytshirt',
-        price: 15,
+        prices: 15,
         inCart:0
     },
     {
-        name: 'Corona Fear',
+        name: 'grey tshirt',
         tag: 'greytshirt',
-        price: 15,
+        prices: 15,
         inCart:0
     }
-]
+];
 
-for (let i=0; i < carts.length; i++){
-    carts(i).addEventListener('click', () =>{
-        cartNumbers(products[1]);
+
+
+for (let i=0; i< carts.length; i++){
+    carts[i].addEventListener('click', () =>{
+    cartNumbers();
     })
 }
-
 function onLoadCartNumbers(){
     let productNumbers = localStorage.getItem('cartNumbers');
 
     if(productNumbers){
-        document.querySelector('.cart span').textContent = productNumbers;
+        document.querySelector('.cart span').textContent = productNumbers
     }
-
 }
-function cartNumbers(products){
-
+function cartNumbers(){
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
-    
-    if(productNumbers){
-        localStorage.setItem('cartNumbers', productNumbers + 1);
-        document.querySelector('.cart span').textContent = products+1;
+
+
+    if(productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers+1);
     }else{
-        localStorage.setItem('cartNumbers', 1);
-        document.querySelector('.cart span').textContent = 1;
+        localStorage.setItem('cartNumbers',1);
+        document.querySelector('.cart span').textContent = 1
     }
-    setItems(product)
-}
-function setItems(product){
-    console.log("inside of SetItems function");
-    console.log("My product is", product);
-    product.inCart = 1;
-    let cartItems ={
-        [product.tag]:product
-    }
-   
-    localStorage.setItem("productsInCart")
+
 }
 onLoadCartNumbers();
